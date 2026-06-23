b3272615a08532fb30a32a024754c36740d232c0
tomasdf96 <tomasdf96@gmail.com>
2026-06-23 21:36:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,24 +10,24 @@ PASSWORD = os.environ.get("PASSWORD")
 birthday_df = pd.read_csv("birthdays.csv")
 birthday_dict = birthday_df.to_dict(orient="records")
 
-adding_birthdays = True
+# adding_birthdays = True
 
-while adding_birthdays:
-    new_entry = input("Do you want to add a birthday to the list? Type 'y' or 'n': ").lower()
-    if new_entry == "y":
-        new_birthday = {
-            "name": input("What is the person's name?\n").title(),
-            "email": input(f"What is their email?\n").lower(),
-            "year": input("Enter year: "),
-            "month": input("Enter month: "),
-            "day": input("Enter day: ")
-        }
-        birthday_dict.append(new_birthday)
-        print("Added a birthday!\n")
-    else:
-        new_birthday_df = pd.DataFrame(birthday_dict)
-        new_birthday_df.to_csv("birthdays.csv", index=False)
-        adding_birthdays = False
+# while adding_birthdays:
+#     new_entry = input("Do you want to add a birthday to the list? Type 'y' or 'n': ").lower()
+#     if new_entry == "y":
+#         new_birthday = {
+#             "name": input("What is the person's name?\n").title(),
+#             "email": input(f"What is their email?\n").lower(),
+#             "year": input("Enter year: "),
+#             "month": input("Enter month: "),
+#             "day": input("Enter day: ")
+#         }
+#         birthday_dict.append(new_birthday)
+#         print("Added a birthday!\n")
+#     else:
+#         new_birthday_df = pd.DataFrame(birthday_dict)
+#         new_birthday_df.to_csv("birthdays.csv", index=False)
+#         adding_birthdays = False
 
 today = dt.datetime.now()
 

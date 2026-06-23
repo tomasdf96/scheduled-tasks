d780fd6ef6452bac43e73338b6c70ee53ad31913
tomasdf96 <tomasdf96@gmail.com>
2026-06-23 21:55:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import random
 import smtplib
 import os
 
-MY_EMAIL = os.environ.get(MY_EMAIL)
-MY_PASSWORD = os.environ.get(MY_PASSWORD)
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 birthday_df = pd.read_csv("birthdays.csv")
 birthday_dict = birthday_df.to_dict(orient="records")
